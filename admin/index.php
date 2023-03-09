e48857d2980d0410fed14e54d690407d8b9d0fda
ALX <alexmonac13@gmail.com>
2023-03-09 11:27:17 +0100

    [ Update ] Security admin page

--- admin/index.php
+++ admin/index.php
@@ -22,30 +22,35 @@
 <body>
     <?php
     session_start();
-    require_once 'Controllers/Controller.php';
-    require_once 'Models/Model.php';
-    require_once 'Utils/header.php';
-    echo "<div class='infos'><h6> Controller : " . $_GET['controller'] . '</h6>' . "<br>";
-    echo "<h6> Action : " . $_GET['action'] . '</h6> </div>' . "<br>";
+    if ($_SESSION['isAdmin']) {
+        require_once 'Controllers/Controller.php';
+        require_once 'Models/Model.php';
+        require_once 'Utils/header.php';
+        echo "<div class='infos'><h6> Controller : " . $_GET['controller'] . '</h6>' . "<br>";
+        echo "<h6> Action : " . $_GET['action'] . '</h6> </div>' . "<br>";
 
-    $controllers = ["home", "livre", "fournisseur", "commande"];
-    $controller_default = "home";
+        $controllers = ["home", "livre", "fournisseur", "commande"];
+        $controller_default = "home";
 
-    if (isset($_GET['controller']) and in_array($_GET['controller'], $controllers)) {
-        $nom_controller = $_GET['controller'];
+        if (isset($_GET['controller']) and in_array($_GET['controller'], $controllers)) {
+            $nom_controller = $_GET['controller'];
+        } else {
+            $nom_controller = $controller_default;
+        }
+
+        $nom_classe = "Controller_" . $nom_controller;
+        $nom_fichier = "Controllers/" . $nom_classe . ".php";
+
+        if (file_exists($nom_fichier)) {
+            require_once($nom_fichier);
+            $controller = new $nom_classe();
+        } else {
+            exit("Error 404 : not found");
+        }
     } else {
-        $nom_controller = $controller_default;
+        header('Location: ../user/?controller=home&action=home');
     }
 
-    $nom_classe = "Controller_" . $nom_controller;
-    $nom_fichier = "Controllers/" . $nom_classe . ".php";
-
-    if (file_exists($nom_fichier)) {
-        require_once($nom_fichier);
-        $controller = new $nom_classe();
-    } else {
-        exit("Error 404 : not found");
-    }
     ?>
 
 </body>
