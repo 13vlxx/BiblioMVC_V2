bee09882895fff28485a5083c171ff95cde8e79f
ALX <alexmonac13@gmail.com>
2023-03-15 13:54:56 +0100

    [ Update ] Sign up anti SQL injection

--- Controllers/Controller_sign_up.php
+++ Controllers/Controller_sign_up.php
@@ -52,33 +52,5 @@ class Controller_sign_up extends Controller
             $this->render("sign_up");
         }
     }
-/*
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-$Nom = $_POST['nom'];
-$Prenom = $_POST['prenom'];
-$Mail = $_POST['mail'];
-$Password = $_POST['password'];
-if (empty($Nom) || empty($Prenom) || empty($Mail) || empty($Password)) {
-echo 'Certains champs sont vides';
-exit;
-}
-if (!filter_var($Mail, FILTER_VALIDATE_EMAIL)) {
-echo 'Format de l\'adresse mail incorrect';
-exit;
-}
-if (!filter_var($Nom, FILTER_SANITIZE_STRING) || !filter_var($Prenom, FILTER_SANITIZE_STRING)) {
-echo 'Format du nom ou du prénom incorrect';
-exit;
-}
-// Utilisation de FILTER_UNSAFE_RAW pour les champs de mot de passe
-if (!filter_var($Password, FILTER_UNSAFE_RAW)) {
-echo 'Format du mot de passe incorrect';
-exit;
-}
-$Nom = trim(htmlspecialchars($Nom));
-$Prenom = trim(htmlspecialchars($Prenom));
-$Mail = trim($Mail);
-$Password = trim(htmlspecialchars($Password));
-*/
 }
 ?>
